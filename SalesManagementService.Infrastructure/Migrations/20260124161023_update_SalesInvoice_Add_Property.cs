a31fb445eefc8306873f72566372258e2d9387bf
123amitsofteng <123amitsofteng@gmail.com>
2026-01-26 23:55:57 +0530

    changes

---
+++ SalesManagementService.Infrastructure/Migrations/20260124161023_update_SalesInvoice_Add_Property.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesManagementService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_SalesInvoice_Add_Property : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TaxPercentage",
+                table: "SalesInvoiceLineItems",
+                type: "numeric(5,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TaxPercentage",
+                table: "SalesInvoiceLineItems");
+        }
+    }
+}
